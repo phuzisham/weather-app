227208b70fac03e5d5d953c226fd227aead0275a
David <david.hale7@gmail.com>
2018-06-04 11:09:22 -0700

    add conversion functions

--- scripts.js
+++ scripts.js
@@ -11,8 +11,17 @@ request.onload = function() {
   var cTemp = temp - 273;
   var fTemp = 9/5 * cTemp + 32;
   console.log('K =', temp);
-  console.log('C =', cTemp);
-  console.log('F =', fTemp);
+  console.log('C =', cTempConversion(temp));
+  console.log('F =', fTempConversion(temp));
+}
+
+var cTempConversion = function(temp) {
+  return (temp - 273).toFixed(0);
+}
+
+var fTempConversion = function(temp) {
+  temp = temp - 273;
+  return (9/5 * temp + 32).toFixed(0);
 }
 
 request.send();
