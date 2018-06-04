043f0e1cb92f8421ba3696d6d13cb1a1989c02d1
David <david.hale7@gmail.com>
2018-06-04 14:21:14 -0700

    toggle between fahrenheit and celcius

--- scripts.js
+++ scripts.js
@@ -1,17 +1,18 @@
 var request = new XMLHttpRequest();
 var temp = 0;
+var cTemp = 0;
+var fTemp = 0;
 var tempDiv = document.getElementById('temp');
 
 request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=portland&APPID=ff3c9f4d39e7e445f23cfbb2e3ba1f27', true);
 
-console.log('outside the payload');
-
-request.onload = function() {
-  console.log('in the payload');
+request.onload = function(temp) {
   var data = JSON.parse(this.response);
-  var temp = data.main.temp;
+  temp = data.main.temp;
+  cTemp = cTempConversion(temp);
+  fTemp = fTempConversion(cTemp);
 
-  tempDiv.innerHTML = cTempConversion(temp);
+  tempDiv.innerHTML = cTemp;
 }
 
 var cTempConversion = function(temp) {
@@ -19,8 +20,15 @@ var cTempConversion = function(temp) {
 }
 
 var fTempConversion = function(temp) {
-  temp = temp - 273;
   return (9/5 * temp + 32).toFixed(0);
+}
+
+var changeUnits = function(temp) {
+  if (tempDiv.innerHTML == cTemp) {
+    tempDiv.innerHTML = fTemp;
+  } else {
+      tempDiv.innerHTML = cTemp;
+  }
 }
 
 request.send();
