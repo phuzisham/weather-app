0730decf8f55a88ecc5e28c14b0d10b676f4682c
David <david.hale7@gmail.com>
2018-06-06 15:45:44 -0700

    displaying weather based on location

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+let request = new XMLHttpRequest();
+let weatherKey = config.weatherKey;
+let temp, cTemp, fTemp, lat, long;
+let tempDiv = document.getElementById('temp');
+let locationDiv = document.getElementById('location');
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      lat = position.coords.latitude.toFixed(3);
+      long = position.coords.longitude.toFixed(3);
+      console.log(lat);
+      console.log(long);
+      getWeather();
+    });
+  } else {
+    locationError();
+  }
+}
+
+function locationError() {
+  console.log('ERROR');
+}
+
+function getWeather() {
+  request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&APPID=' + weatherKey, true);
+
+  request.onload = function() {
+    let data = JSON.parse(this.response);
+    console.log(data);
+    temp = data.main.temp;
+    cTemp = cTempConversion(temp);
+    fTemp = fTempConversion(cTemp);
+    city = data.name;
+
+    locationDiv.innerHTML = city + ' Weather';
+    tempDiv.innerHTML = cTemp + ' Degrees Celcius.';
+  }
+  request.send();
+}
+
+function cTempConversion(temp) {
+  return (temp - 273).toFixed(0);
+}
+
+function fTempConversion(temp) {
+  return (9/5 * temp + 32).toFixed(0);
+}
+
+function changeUnits() {
+  if (tempDiv.innerHTML == cTemp + ' Degrees Celcius.') {
+    tempDiv.innerHTML = fTemp + ' Degrees Fahrenheit.';
+  } else {
+      tempDiv.innerHTML = cTemp + ' Degrees Celcius.';
+  }
+}
+
+getLocation();
